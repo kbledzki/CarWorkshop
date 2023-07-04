104f7add2a211c42826eb6ddd2106d7ef50ff0c4
kbledzki <krzysztofbledzki@gmail.com>
2023-07-04 13:23:33 +0200

    add mapper

--- src/main/java/com/kb/carWorkshop/repository/VehicleRepository.java
+++ src/main/java/com/kb/carWorkshop/repository/VehicleRepository.java
@@ -5,11 +5,12 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
 import java.util.UUID;
 
+@Repository
 public class VehicleRepository extends JpaRepository <Vehicle, UUID> {
 
 
@@ -25,12 +26,9 @@ public class VehicleRepository extends JpaRepository <Vehicle, UUID> {
     @Override
     List <Vehicle> findVehicleByFixedIsFalse();
 
-    @Transactional
-    @Modifying
-    @Query("update Vehicle v set v.isFixed = ?1 where v.id = ?2")
-    void updateVehicle(boolean fixed, UUID id);
-
-
-
-
+//    @Transactional
+//    @Modifying
+//    @Query("update Vehicle v set v.isFixed = ?1 where v.id = ?2")
+//    void updateVehicle(boolean fixed, UUID id) {
+//    }
 }
--- src/main/java/com/kb/carWorkshop/service/VehicleService.java
+++ src/main/java/com/kb/carWorkshop/service/VehicleService.java
@@ -1,6 +1,17 @@
 package com.kb.carWorkshop.service;
 
+import com.kb.carWorkshop.repository.VehicleRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class VehicleService {
+
+    private final VehicleRepository vehicleRepository;
+
+    public VehicleService(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+
+
 }
