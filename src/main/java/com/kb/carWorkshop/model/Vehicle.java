9265ec2bb2f72e06f96210e9e389e6eece24245e
kbledzki <krzysztofbledzki@gmail.com>
2023-07-04 12:51:30 +0200

    add entity vehicle

--- src/main/java/com/kb/carWorkshop/model/Vehicle.java
+++ src/main/java/com/kb/carWorkshop/model/Vehicle.java
@@ -1,24 +1,44 @@
 package com.kb.carWorkshop.model;
 
 import com.kb.carWorkshop.enums.Color;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
 
 import java.util.UUID;
 
+@Entity
+@Table(name = Vehicle.TABLE_NAME)
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Vehicle {
+
     public static final String TABLE_NAME = "vehicle";
+    public static final String COLUMN_PREFIX = "v_";
+
+    @Id
+    @GeneratedValue
+    @Column(name = COLUMN_PREFIX + "id")
     private UUID id;
+
+    @Column(name = COLUMN_PREFIX + "name", nullable = false)
     private String name;
+
+    @Column(name = COLUMN_PREFIX + "registrationNumber", nullable = false)
     private String registrationNumber;
+
+    @Column(name = COLUMN_PREFIX + "isFixed")
     private boolean isFixed;
+
+    @Column(name = COLUMN_PREFIX + "color")
     private Color color;
+
+    @Column(name = COLUMN_PREFIX + "productionDate")
     private int productionDate;
 
 }
--- src/main/java/com/kb/carWorkshop/repository/VehicleRepository.java
+++ src/main/java/com/kb/carWorkshop/repository/VehicleRepository.java
@@ -2,9 +2,14 @@ package com.kb.carWorkshop.repository;
 
 import com.kb.carWorkshop.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 import java.util.UUID;
 
 public class VehicleRepository extends JpaRepository <Vehicle, UUID> {
+
+    @Override
+    List<Vehicle> findVehicleByName(String name);
+
 
 }
