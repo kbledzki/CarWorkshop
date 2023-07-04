513a5c45ce4f0b6977171fda6aae8f0cf7383583
kbledzki <krzysztofbledzki@gmail.com>
2023-07-04 12:57:15 +0200

    add methods to repository

--- src/main/java/com/kb/carWorkshop/repository/VehicleRepository.java
+++ src/main/java/com/kb/carWorkshop/repository/VehicleRepository.java
@@ -1,15 +1,36 @@
 package com.kb.carWorkshop.repository;
 
 import com.kb.carWorkshop.model.Vehicle;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 import java.util.UUID;
 
 public class VehicleRepository extends JpaRepository <Vehicle, UUID> {
 
+
     @Override
     List<Vehicle> findVehicleByName(String name);
+
+    @Override
+    List<Vehicle> findVehicleByRegistrationNumberAndAndFixedIsTrue(String registrationNumber);
+
+    @Override
+    List<Vehicle> findVehicleByFixedIsTrue();
+
+    @Override
+    List <Vehicle> findVehicleByFixedIsFalse();
+
+    @Transactional
+    @Modifying
+    @Query("update Vehicle v set v.isFixed = ?1 where v.id = ?2")
+    void updateVehicle(boolean fixed, UUID id);
+
+
 
 
 }
